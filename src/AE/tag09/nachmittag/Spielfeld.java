a2a30877a14c453e0ed423aa860be64f2066194f
FBaumgarten <fbau116@gmail.com>
2019-09-10 09:16:39 +0200

    update

--- src/AE/tag09/nachmittag/Spielfeld.java
+++ src/AE/tag09/nachmittag/Spielfeld.java
@@ -5,11 +5,8 @@ public class Spielfeld {
     private Zelle[][] spielefeldArray = new Zelle[SIZE_X][SIZE_Y];
 
     public void initSpielfeld() {
-        for (int x = 0; x < spielefeldArray.length; x++) {
-            for (int y = 0; y < spielefeldArray[y].length; y++) {
-                spielefeldArray[x][y] = new Zelle(Farbe.randomFarbe(), x, y);
-            }
-        }
+        fuelleFarben();
+        fuelleNachbarn();
 
 //        for (Zelle[] row:spielefeldArray) {
 //            for (Zelle zelle:row) {
@@ -19,10 +16,33 @@ public class Spielfeld {
 //        }
     }
 
+    private void fuelleNachbarn() {
+        for (int x = 0; x < spielefeldArray.length; x++) {
+            for (int y = 0; y < spielefeldArray[x].length; y++) {
+                if (x > 0) spielefeldArray[x][y].setNord(spielefeldArray[x-1][y]);
+                else spielefeldArray[x][y].setNord(null);
+                if (x < spielefeldArray.length - 1) spielefeldArray[x][y].setSued(spielefeldArray[x + 1][y]);
+                else spielefeldArray[x][y].setSued(null);
+                if (y > 0) spielefeldArray[x][y].setWest(spielefeldArray[x][y-1]);
+                else spielefeldArray[x][y].setWest(null);
+                if (y < spielefeldArray[x].length - 1) spielefeldArray[x][y].setOst(spielefeldArray[x][y+1]);
+                else spielefeldArray[x][y].setOst(null);
+            }
+        }
+    }
+
+    private void fuelleFarben() {
+        for (int x = 0; x < spielefeldArray.length; x++) {
+            for (int y = 0; y < spielefeldArray[x].length; y++) {
+                spielefeldArray[x][y] = new Zelle(Farbe.randomFarbe(), x, y);
+            }
+        }
+    }
+
     public void printSpielfeld() {
-        for (int y = 0; y < spielefeldArray.length; y++) {
+        for (int x = 0; x < spielefeldArray.length; x++) {
             System.out.println();
-            for (int x = 0; x < spielefeldArray[y].length; x++) {
+            for (int y = 0; y < spielefeldArray[x].length; y++) {
                 System.out.print("\t" + spielefeldArray[x][y].getZellenFarbe());
             }
         }
