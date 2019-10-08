b425185dfdc8fd16a878bf75cdd3ddba89de49c6
Frank Baumgarten <baumgafr@gmail.com>
2019-10-08 19:54:39 +0200

    update

--- src/AE2/password/PWGenTest.java
+++ src/AE2/password/PWGenTest.java
@@ -1,10 +1,11 @@
 package AE2.password;
 
 public class PWGenTest {
+    static final String FILEPATH = "C:\\data\\lewdlist.txt";
     public static void main(String[] args) {
-        PasswordGenerator pwgen = new PasswordGenerator(FileOperations.readFile("C:\\data\\lewdlist.txt"));
+        PasswordGenerator pwgen = new PasswordGenerator(FileOperations.readFile(FILEPATH));
         for (int i = 0; i < 10; i++) {
-            System.out.println(pwgen.generatePassword(20));
+            System.out.println(pwgen.generatePassword(12, 0.2f, 0.4f));
         }
     }
 }
--- src/AE2/password/PasswordGenerator.java
+++ src/AE2/password/PasswordGenerator.java
@@ -19,6 +19,94 @@ public class PasswordGenerator {
         setWordlist(stringArrayList);
     }
 
+    String leetify(String string, double percent){
+        int r = (int) (string.length() * percent);
+        boolean[] toLeetArray = new boolean[string.length()];
+        Random random = new Random();
+        int x = random.nextInt(string.length());
+        for (int j = 0; j < r; j++) {
+            while (toLeetArray[x]) x = random.nextInt(string.length());
+            toLeetArray[x] = true;
+        }
+        char[] result =string.toCharArray();
+        for (int j = 0; j < string.length(); j++) {
+            if (toLeetArray[j]){
+                switch (result[j]){
+                    case 'a':
+                        result[j] = '4';
+                        break;
+                    case 'b':
+                        result[j] = '8';
+                        break;
+                    case 'c':
+                        result[j] = '(';
+                        break;
+                    case 'e':
+                        result[j] = '3';
+                        break;
+                    case 'g':
+                        result[j] = '6';
+                        break;
+                    case 'h':
+                        result[j] = '#';
+                        break;
+                    case 'i':
+                        result[j] = '!';
+                        break;
+                    case 'l':
+                        result[j] = '1';
+                        break;
+                    case 'o':
+                        result[j] = '0';
+                        break;
+                    case 'p':
+                        result[j] = '9';
+                        break;
+                    case 'r':
+                        result[j] = '2';
+                        break;
+                    case 's':
+                        result[j] = '5';
+                        break;
+                    case 't':
+                        result[j] = '7';
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        return new String(result);
+    }
+
+    String capitalize(String string, double percent){
+        int r = (int) (string.length() * percent);
+        boolean[] toCapitalizeArray = new boolean[string.length()];
+        Random random = new Random();
+        int x = random.nextInt(string.length());
+        for (int j = 0; j < r; j++) {
+            while (toCapitalizeArray[x]) x = random.nextInt(string.length());
+            toCapitalizeArray[x] = true;
+        }
+        char[] result =string.toCharArray();
+        for (int j = 0; j < string.length(); j++) {
+            if (toCapitalizeArray[j]){
+                result[j] = Character.toUpperCase(result[j]);
+            }
+        }
+
+        return new String(result);
+    }
+
+    public String generatePassword(int length, double percent){
+        return leetify(generatePassword(length), percent);
+    }
+
+    public String generatePassword(int lenght, double leetifyPercent, double capitalizePercent){
+        return capitalize(leetify(generatePassword(lenght),leetifyPercent),capitalizePercent);
+    }
+
     public String generatePassword(int lenght){
         StringBuilder result = new StringBuilder();
         ArrayList<String> temp = new ArrayList<>();
